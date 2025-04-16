cc6eca4ce428987279e61dece10ca57cf54e7c5a
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-04-17 00:14:17 +0300

    feat: add CallToAction component and integrate it into the Landing Page

---
+++ src/components/CallToAction.tsx
@@ -0,0 +1,9 @@
+const CallToAction = () => {
+  return (
+    <div className="flex flex-col items-center justify-center p-24">
+      <h3 className="text-2xl font-bold">Call To Action</h3>
+    </div>
+  );
+};
+
+export default CallToAction;
