8d7d53d98ffa33fe885d16f9c9fb7f0942942fa1
Brayarn Ogeda <brayarnogeda@gmail.com>
2025-04-29 16:07:53 +0300

    feat: add volunteer & donate components to get-involved page

--- src/app/get-involved/page.tsx
+++ src/app/get-involved/page.tsx
@@ -1,9 +1,14 @@
+import Donate from '@/components/donate';
+import Volunteer from '@/components/volunteer';
 import React from 'react';
 
 export default function GetInvolved() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">Get Involved</h1>
+
+      <Volunteer />
+      <Donate />
     </main>
   );
 }
