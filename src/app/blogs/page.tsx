cd80c8def597d59589e014d233186f6af8e9105f
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-05-16 01:09:38 +0300

    Merge branch 'development' into feature/blog-display

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -5,15 +5,15 @@ import Pagination from './Pagination';
 
 export default function BlogsPage() {
   return (
-    <main className="flex flex-col items-center justify-between mx-8">
+    <main className="flex flex-col justify-center">
       <BlogWelcomeHeader />
-
-      <div className="w-full px-12 text-tsk-primary-dark mt-10">
-        <h3 className="font-body text-xl font-bold mb-5">Categories</h3>
-        <TagSelector />
+      <div className="flex flex-col items-center justify-between mx-8">
+        <div className="w-full px-12 text-tsk-primary-dark mt-10">
+          <h3 className="font-body text-xl font-bold mb-5">Categories</h3>
+          <TagSelector />
+        </div>
+        <Pagination />
       </div>
-
-      <Pagination />
     </main>
   );
 }
