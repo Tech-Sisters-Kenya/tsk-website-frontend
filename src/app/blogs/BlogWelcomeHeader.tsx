69c0b5a1d27425278cd5441f5fce148c8aa0ea70
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-06-19 15:31:44 +0300

    Refactor BlogWelcomeHeader layout and adjust styling

--- src/app/blogs/BlogWelcomeHeader.tsx
+++ src/app/blogs/BlogWelcomeHeader.tsx
@@ -4,17 +4,17 @@ import { AnimatedBlogPage } from '../../components/svg_components/AnimatedBlogPa
 const BlogWelcomeHeader = () => {
   return (
     <section className="w-full  mx-auto text-tsk-primary-dark px-8 -mt-14 -z-10">
-      <div className=" bg-tsk-light-2 rounded-b-3xl px-16">
-        <div className="pt-20 grid grid-cols-1 lg:grid-cols-2 items-center">
+      <div className=" bg-tsk-light-2 rounded-b-3xl px-14">
+        <div className="grid grid-cols-1 lg:grid-cols-2 items-center">
           <div>
             <h1 className="font-black font-heading text-5xl leading-[150%]">Blogs</h1>
-            <div className="font-body text-[32px] mt-8">
+            <div className="font-body text-[30px] font-medium mt-8">
               <p>Welcome to the Tech Sisters Kenya blog </p>
               <p> — </p>
               <p className=""> A space created to educate, inspire, and uplift women in tech. </p>
             </div>
           </div>
-          <div className="flex justify-center items-center w-full">
+          <div className="flex w-full">
             <AnimatedBlogPage />
           </div>
         </div>
