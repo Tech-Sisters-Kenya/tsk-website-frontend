93fff51cc38b0281214a6a019597067396a83855
StellaWanja <stylades@gmail.com>
2025-06-26 16:00:22 +0300

    feat: implement blogs API and filtering

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import BlogWelcomeHeader from '@/app/blogs/BlogWelcomeHeader';
 import TagSelector from '@/app/blogs/TagSelector';
-import Pagination from './Pagination';
 
 export default function BlogsPage() {
   return (
-    <main className="flex flex-col justify-center">
+    <main className="flex flex-col justify-center py-10">
       <BlogWelcomeHeader />
       <div className="flex flex-col items-center justify-between mx-8">
         <div className="w-full px-12 text-tsk-primary-dark mt-10">
           <h3 className="font-body text-xl font-bold mb-5">Categories</h3>
           <TagSelector />
         </div>
-        <Pagination />
       </div>
     </main>
   );
