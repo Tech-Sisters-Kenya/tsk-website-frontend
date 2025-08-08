2171509f47e51cdd1fcc7d78186b1f433a2a72ec
mercie-ux <mbaomercy0@gmail.com>
2025-08-08 08:47:01 +0300

    fix: modified jest setup file

--- jest.setup.js
+++ jest.setup.js
@@ -1,14 +1,1 @@
-/* global jest */
-import React from 'react';
 import '@testing-library/jest-dom';
-
-// mock next/image
-jest.mock('next/image', () => ({
-  __esModule: true,
-  default: (props) => {
-    return React.createElement('img', {
-      ...props,
-      alt: props.alt || 'mocked image',
-    });
-  },
-}));
